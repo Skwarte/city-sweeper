92789fe585e776a51907f7e256a30a5956511e2f
skwarte <skwarte@purdue.edu>
2018-11-07 01:02:51 -0600

    added html/css to webpage.

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ path = require("path");
 
 app = express();
 
+app.use("/public", express.static(path.resolve(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 app.post("/", function(req, res) {
+  console.log("Post sent from /");
   var button = req.body.button;
   if (button === "Learn More") {
     res.sendFile(__dirname + "/views/about.html");
@@ -20,6 +21,6 @@ app.post("/", function(req, res) {
     res.redirect("/");
   }
 });
-app.listen(2995, function() {
-  console.log("Server running on port 3000");
+app.listen(4000, function() {
+  console.log("Server running on port 4000");
 });
